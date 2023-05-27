64e2e0d399a2e6a63eecada523d53448f9931c30
LaykTaviskaron <layk.taviskaron@gmail.com>
2023-05-27 12:02:08 +0300

    Struct and boxing

---
+++ ConsoleApp3/ConsoleApp1/Point.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+public struct Point
+{
+    int X;
+    int Y;
+    int Z;
+
+    public Point(int x, int y, int z)
+    {
+        this.X = x;
+        this.Y = y;
+        this.Z = z;
+    }
+
+    public static Point operator+(Point first, Point second)
+    {
+        return new Point(first.X + second.X, first.Y + second.Y, first.Z + second.Z);
+    }
+
+    public static Point operator -(Point first, Point second)
+    {
+        return new Point(first.X - second.X, first.Y - second.Y, first.Z - second.Z);
+    }
+
+    public static Point operator *(Point first, Point second)
+    {
+        return new Point(first.X * second.X, first.Y * second.Y, first.Z * second.Z);
+    }
+
+    public static Point Multiply(Point first, Point second)
+    {
+        return new Point(first.X * second.X, first.Y * second.Y, first.Z * second.Z);
+    }
+}
+
--- ConsoleApp3/ConsoleApp1/Program.cs
+++ ConsoleApp3/ConsoleApp1/Program.cs
@@ -1,5 +1,47 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, Team!");
-Console.WriteLine("First line");
-Console.WriteLine("Second line");
+
+var struct1 = new Point(10, 12, 0);
+var struct2 = new Point(12, 10, 0);
+
+var struct3 = Point.Multiply(struct1, struct2);
+
+var user = new User
+{
+    Name = "Test",
+    Password = "qwerty"
+};
+
+
+int i = 100;
+long l = 100;
+
+int intFromLong = (int)l;
+
+object intObj = (object)i;
+int intFromObj = (int)intObj;
+
+object longObj = (object)l;
+int intFromLongObj = (int)longObj;
+
+var userType = user.GetType();
+object userObject = (object)user;
+var userObjectType = userObject.GetType();
+
+userObject = (object)1200;
+
+if (userObject is int)
+{
+    var intFromObject = (int)userObject;
+}
+
+if (userObject is User)
+{
+
+    var userFromObject = (User)userObject;
+}
+
+
+Console.ReadLine();
