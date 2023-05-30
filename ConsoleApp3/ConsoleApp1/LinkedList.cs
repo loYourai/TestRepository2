83dc3596dab1238cd5372615341de028eddffe8c
LaykTaviskaron <layk.taviskaron@gmail.com>
2023-05-30 21:06:39 +0300

    Added generics and list

---
+++ ConsoleApp3/ConsoleApp1/LinkedList.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class LinkedList<T>
+    {
+        private Item head;
+
+        // add, get(index), clear, insert
+        // lenght/count
+
+        public LinkedList()
+        {
+            Length = 0;
+        }
+
+        public int Length { get; private set; }
+
+        public void Insert(T element, int index)
+        {
+            if (index < 1 && index > Length)
+            {
+                throw new ArgumentException("Such index doesn't exist");
+            }
+
+            if (head == null)
+            {
+                throw new ArgumentException("Such index doesn't exist");
+            }
+
+            var currentItem = head;
+            int i = 1;
+            while (i++ < index-1 && currentItem != null)
+            {
+                currentItem = currentItem.Next;
+            }
+
+            if (currentItem == null) // when 5 items and requested 10
+            {
+                throw new ArgumentException("Such index doesn't exist");
+            }
+
+            var newItem = new Item
+            {
+                Value = element,
+                Next = null
+            };
+
+            if (index == 1)
+            {
+                newItem.Next = head;
+                head = newItem;
+            }
+            else
+            {
+                var nextItem = currentItem.Next;
+                currentItem.Next = newItem;
+                newItem.Next = nextItem;
+            }
+
+            Length++;
+        }
+
+        public int Count()
+        {
+            if (head == null)
+            {
+                return 0;
+            }
+
+            var currentItem = head;
+            int i = 0;
+            while (currentItem != null)
+            {
+                i++;
+                currentItem = currentItem.Next;
+            }
+
+            return i;
+        }
+
+        public void Add(T element)
+        {
+            if (head == null) // count of elements = 0
+            {
+                head = new Item
+                {
+                    Value = element,
+                    Next = null
+                };
+            }
+            else
+            {
+                var nextElement = head.Next;
+                var currentElement = head;
+                while (nextElement != null)
+                {
+                    currentElement = nextElement;
+                    nextElement = nextElement.Next;
+                }
+
+                var newItem = new Item
+                {
+                    Value = element,
+                    Next = null
+                };
+
+                currentElement.Next = newItem;
+            }
+
+            Length++;
+        }
+
+        public T Get(int index)
+        {
+            if (index < 1)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (head == null)
+            {
+                throw new ArgumentException("Such index doesn't exist");
+            }
+
+            var currentItem = head;
+            int i = 1;
+            while (i++ < index && currentItem != null)
+            {
+                currentItem = currentItem.Next;
+            }
+
+            if (currentItem == null) // when 5 items and requested 10
+            {
+                throw new ArgumentException("Such index doesn't exist");
+            }
+
+            return currentItem.Value;
+        }
+
+        public void Clear()
+        {
+            head = null;
+            Length = 0;
+        }
+
+        private class Item
+        {
+            public T Value { get; set; }
+
+            public Item Next { get; set; }
+        }
+    }
+}
--- ConsoleApp3/ConsoleApp1/Program.cs
+++ ConsoleApp3/ConsoleApp1/Program.cs
@@ -1,5 +1,67 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, Team!");
-Console.WriteLine("First line");
-Console.WriteLine("Second line");
+
+//int first = 10;
+//int second = 20;
+
+
+//string str1 = "123";
+//string str2 = "456";
+
+//string Print<U1, U2>(U1 param1, U2 param2)
+//where U1 : class
+//where U2 : class
+//{
+//    return param1.ToString() + param2.ToString();
+//}
+
+//string Print<U1, U2, U3>(U1 param1, U2 param2, U3 param3)
+//    where U1 : class
+//    where U2 : class
+//    where U3 : class
+//{
+//    return param1.ToString() + param2.ToString() + param3.ToString();
+//}
+
+
+////Print(10, 20);
+//Console.WriteLine(Print(str1, str2));
+
+
+var strings = new ConsoleApp1.LinkedList<string>();
+strings.Add("1");
+strings.Add("2");
+strings.Add("3");
+strings.Add("4");
+strings.Add("5");
+//strings.Insert("6", 6);
+//strings.Insert("100", 2);
+//strings.Insert("200", 4);
+
+strings.Insert("0", 1);
+
+var count = strings.Count();
+var lenght = strings.Length;
+
+//strings.Clear();
+//strings.Add("10");
+//var count1 = strings.Count();
+//var lenght1 = strings.Length;
+
+Console.ReadLine();
+//var fourhtElement = strings.Get(4);
+//Console.WriteLine(fourhtElement);
+
+//var integers = new ConsoleApp1.LinkedList<int>();
+//integers.Add(1);
+//integers.Add(2);
+//integers.Add(3);
+//integers.Add(4);
+//integers.Add(5);
+
+//var fourhtIntElement = integers.Get(3);
+//Console.WriteLine(fourhtIntElement);
+
+//Console.WriteLine(str1);
+//Console.WriteLine(str2);
