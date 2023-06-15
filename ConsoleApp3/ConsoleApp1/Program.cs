9f9979ff550490599d810c903571887affd71d61
LaykTaviskaron <layk.taviskaron@gmail.com>
2023-06-15 18:32:57 +0300

    Reflection

--- ConsoleApp3/ConsoleApp1/Program.cs
+++ ConsoleApp3/ConsoleApp1/Program.cs
@@ -1,5 +1,70 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+using System.Reflection;
+
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, Team!");
-Console.WriteLine("First line");
-Console.WriteLine("Second line");
+
+int i = 10;
+var typeInfo = typeof(int);
+
+//Console.WriteLine(typeInfo.DeclaringType);
+//Console.WriteLine(typeInfo.BaseType);
+//Console.WriteLine(typeInfo.FullName);
+
+//var objectFromActivator = Activator.CreateInstance(typeInfo);
+//if (typeInfo.Name == "Test")
+//{
+//    var newTest1 = objectFromActivator as Test;
+//}
+
+//var newTest = objectFromActivator as Test;
+//if (newTest != null)
+//{
+
+//}
+
+
+var test = new Test();
+
+var privateMethods = Type.GetType("ConsoleApp1.Test")
+                          .GetProperties(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Static);
+
+foreach (var property in Type.GetType("ConsoleApp1.Test")
+                          .GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Static))
+{
+    var hasDefaultValueAttr = false;
+    int? defaultValueAttr = 0;
+    foreach (var attr in property.CustomAttributes)
+    {
+        hasDefaultValueAttr = attr.AttributeType == typeof(DefaultValue);
+        defaultValueAttr = attr.ConstructorArguments[0].Value as int?;
+
+        if (hasDefaultValueAttr)
+        {
+            break;
+        }
+    }
+
+    if (hasDefaultValueAttr && property.CanWrite)
+    {
+        property.SetValue(test, defaultValueAttr);
+    }
+}
+
+
+
+Console.ReadLine();
+
+// Create new instance
+//obj = (MyFancyObject)Activator.CreateInstance("MyAssembly", ClassName))
+
+// Get all public methods
+//MethodInfo[] methodInfos = Type.GetType(selectedObjcClass)
+//                           .GetMethods(BindingFlags.Public | BindingFlags.Instance);
+
+
+// get methods with attributes
+//var methods = assembly.GetTypes()
+//                      .SelectMany(t => t.GetMethods())
+//                      .Where(m => m.GetCustomAttributes(typeof(MenuItemAttribute), false).Length > 0)
+//                      .ToArray();
---
+++ ConsoleApp3/ConsoleApp1/Test.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    [System.AttributeUsage(System.AttributeTargets.All)]
+    public class DefaultValue : Attribute 
+    {
+        public DefaultValue(int value) 
+        {
+            Value = value;
+        }
+
+        public int Value { get; set; }
+    }
+
+    [DefaultValue(10)]
+    public class Test
+    {
+        [DefaultValue(20)]
+        public int AnotherValue { get; set; }
+
+        [DefaultValue(10)]
+        public int Value { get; set;  }
+
+        public string Name { get; }
+
+        private string PrivateName { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine(Value);
+        }
+
+        private bool Print([DefaultValue(20)] int a)
+        {
+            Console.WriteLine(a);
+            return true;
+        }
+    }
+}
